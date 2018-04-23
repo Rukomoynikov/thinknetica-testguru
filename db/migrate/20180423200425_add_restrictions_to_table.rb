990bd2863ed7210083a805159daeb012d108a718
Maksim R <cat@mos-it.com>
2018-04-23 23:18:04 +0300

    Ограничения целостности на уровне БД

---
+++ db/migrate/20180423200425_add_restrictions_to_table.rb
@@ -0,0 +1,23 @@
+class AddRestrictionsToTable < ActiveRecord::Migration[5.2]
+  def up
+  	change_column :categories, :title, :string, null: false
+  	change_column :categories, :title, :string, null: false
+
+  	change_column :tests, :title, :string, null: false
+  	change_column :tests, :level, :integer, null: false
+  	change_column :tests, :category_id, :integer, null: false
+
+  	change_column :questions, :body, :text, null: false
+  	change_column :questions, :test_id, :integer, null: false
+
+  	change_column :answers, :body, :text, null: false
+  	change_column :answers, :correct, :boolean, null: false
+  	change_column :answers, :question_id, :integer, null: false
+
+  	change_column :users, :name, :string, null: false
+  end
+
+  def down
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_23_195602) do
+ActiveRecord::Schema.define(version: 2018_04_23_200425) do
 
   create_table "answers", force: :cascade do |t|
-    t.text "body"
-    t.integer "questions_id"
+    t.text "body", null: false
+    t.boolean "correct", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["questions_id"], name: "index_answers_on_questions_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "body"
-    t.integer "question_id"
+    t.text "body", null: false
+    t.integer "test_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_questions_on_question_id"
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
-    t.integer "level"
-    t.integer "categories_id"
+    t.string "title", null: false
+    t.integer "level", null: false
+    t.integer "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["categories_id"], name: "index_tests_on_categories_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
