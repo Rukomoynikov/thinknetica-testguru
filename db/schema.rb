45f01066db3bb56081a572ff5c68ac839d8e40e3
Maksim R <cat@mos-it.com>
2018-04-30 19:31:34 +0300

    Исправление ассоциаций по комментариям

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_29_191900) do
+ActiveRecord::Schema.define(version: 2018_04_29_184405) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -18,9 +18,7 @@ ActiveRecord::Schema.define(version: 2018_04_29_191900) do
     t.integer "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
