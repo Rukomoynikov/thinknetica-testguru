c5cc5da3795933e2ce6db132689be425dd7c4674
Maksim R <cat@mos-it.com>
2018-05-14 18:08:42 +0300

    Роут ответов

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :tests do
-    resources :questions, shallow: true
+  root to: 'tests#index'
+  
+  resources :tests, shallow: true do
+    resources :questions do
+      resources :answers
+    end
   end
 end
