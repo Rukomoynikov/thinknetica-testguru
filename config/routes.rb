4b21ac03db0deb53579f4e98532761d384625cbd
Maksim R <cat@mos-it.com>
2018-05-15 23:37:13 +0300

    Scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'tests#index'
   
   resources :tests, shallow: true do
-    resources :questions do
+      resources :questions, except: [:index] do
       resources :answers
     end
   end
