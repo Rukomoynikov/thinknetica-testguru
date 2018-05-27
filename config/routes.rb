c9179b81e769cd2c30dd43dcc9c124a6f85e77d5
Maksim R <cat@mos-it.com>
2018-05-27 15:14:41 +0300

    Закончил код по лекции

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   root to: 'tests#index'
   
   resources :tests, shallow: true do
+      post :start, on: :member
       resources :questions, except: [:index] do
-      resources :answers
+      resources :answers, except: :index
     end
+  end
+
+  resources :test_passages, only: [:show, :update] do
+    get 'result', on: :member
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+User.destroy_all
+Category.destroy_all
+Test.destroy_all
+Question.destroy_all
+Answer.destroy_all
+
 User.create([
   { name: 'John', email: 'john@mail.ru' },
   { name: 'Mary', email: 'mary@mail.ru' },
@@ -6,7 +12,8 @@ User.create([
 Category.create([
 	{ title: 'html' },
 	{ title: 'css' },
-	{ title: 'js' }
+  { title: 'js' },
+  { title: 'ruby' }
 ])
 
 Test.create([
@@ -18,19 +25,13 @@ Test.create([
 	{ title: 'CSS LEVEL 3', level: 3, category_id: Category.second.id, author_id: User.first.id },
 	{ title: 'JS LEVEL 1', level: 1, category_id: Category.third.id, author_id: User.first.id },
 	{ title: 'JS LEVEL 2', level: 2, category_id: Category.third.id, author_id: User.first.id },
-	{ title: 'JS LEVEL 3', level: 3, category_id: Category.third.id, author_id: User.first.id }
+  { title: 'JS LEVEL 3', level: 3, category_id: Category.third.id, author_id: User.first.id },
+  { title: 'Ruby Language', author: User.first, category: Category.last }
 ])
 
 Question.create([
-	{ body: 'Tag of Header ?', test_id: Test.first.id },
-	{ body: 'Tag of Header ?', test_id: Test.first.id },	
-	{ body: 'Tag of Header ?', test_id: Test.first.id },	
-	{ body: 'Tag of Header ?', test_id: Test.first.id },	
-	{ body: 'Tag of Header ?', test_id: Test.second.id },	
-	{ body: 'Tag of Header ?', test_id: Test.second.id },	
-	{ body: 'Tag of Header ?', test_id: Test.second.id },		
-	{ body: 'Tag of Header ?', test_id: Test.third.id },		
-	{ body: 'Tag of Header ?', test_id: Test.third.id }
+  { body: 'Who author of Ruby lang?', test: Test.last },
+  { body: 'Default value of instance variable?', test: Test.last }
 ])
 
 Answer.create([
@@ -38,8 +39,12 @@ Answer.create([
   { body: 'No', question_id: Question.first.id },
   { body: 'Maybe', question_id: Question.first.id },
   { body: 'Correct', question_id: Question.first.id, correct: true },
+  { body: 'Matz', question: Question.first, correct: true },
+  { body: 'Bill Gates', question: Question.first, correct: false },
+  { body: 'nil', question: Question.last, correct: true },
+  { body: 'undefined', question: Question.last, correct: false },
 ])
 
-TestPassage.create([{ user_id: User.first.id, test_id: Test.first.id, finished: false }])
-TestPassage.create([{ user_id: User.first.id, test_id: Test.second.id, finished: true }])
-TestPassage.create([{ user_id: User.first.id, test_id: Test.third.id, finished: true }])
+# TestPassage.create([{ user_id: User.first.id, test_id: Test.first.id, finished: false }])
+# TestPassage.create([{ user_id: User.first.id, test_id: Test.second.id, finished: true }])
+# TestPassage.create([{ user_id: User.first.id, test_id: Test.third.id, finished: true }])
