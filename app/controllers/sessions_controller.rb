f71a499aae37e76a9a0155682b3b42844008753d
Maksim R <cat@mos-it.com>
2018-06-02 21:25:20 +0300

    Flash сообщения

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Are you guru? Identify yourself!'
       render :new
     end
   end
