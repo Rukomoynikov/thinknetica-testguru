766ae7949f535ba9cd34e7e8f2949b5ca47c2fc1
Maksim R <cat@mos-it.com>
2018-06-17 22:03:01 +0300

    Усложненное задание

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -11,5 +12,11 @@ class ApplicationController < ActionController::Base
     end
 
     path
+
+    flash[:info] = "Привет, #{ resource.first_name } #{ resource.last_name }!"
+  end
+
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
